d613c8f9d2bd944a5ba1864b8306ec3397087616
Sarasmishra <140342857+Sarasmishra@users.noreply.github.com>
2024-07-18 20:53:48 +0530

    Add files via upload

---
+++ Level2_nodeBasic/index.js
@@ -0,0 +1,53 @@
+
+const fs = require("node:fs");
+
+const operation = process.argv[2];
+const file = process.argv[3];
+const content = process.argv[4];
+
+switch (operation) {
+  // complete the fillowing function.
+  case 'read':
+    fs.readFile(file,"utf8",(err,data)=>{
+        if(err) console.log(err);
+        else console.log(data);
+    })
+    break;
+    
+    case 'append':
+        fs.appendFile(file,content,(err)=>{
+            if(err) console.log(err);
+            else console.log("Content appended to the file",`${file}`)
+        })
+        break;
+    
+    case 'delete':
+        fs.unlink(file,(err)=>{
+            if(err) console.log(err);
+            else console.log("file",`${file}`,"deleted")
+        })  
+        break;
+        
+     case 'create':
+        fs.writeFile(file,content,(err)=>{
+            if(err) console.log(err);
+            else console.log("File ",`${file}`," created");
+        })
+        break;   
+
+     case 'rename':
+        fs.rename(file,content,(err)=>{
+            if(err) console.log(err);
+            else console.log("File",`${file}`,'renamed to',`${content}`);
+        })   
+        break;
+
+     case 'list':
+        fs.readdir(file,(err,files)=>{
+            if(err) console.log(err)
+            else console.log(files)    
+        })   
+        break;
+  default:
+    console.log(`Invalid operation '${operation}'`);
+}
